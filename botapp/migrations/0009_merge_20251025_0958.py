d292990a8ad629a339cf3c93a4d3041616e5471b
Berik <berik.bekishev@gmail.com>
2025-10-25 10:00:06 +0500

    Add merge migration to resolve conflict

---
+++ botapp/migrations/0009_merge_20251025_0958.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("botapp", "0007_update_video_defaults"),
+        ("botapp", "0008_update_veo_model_name"),
+    ]
+
+    operations: list[migrations.Operation] = []
+
